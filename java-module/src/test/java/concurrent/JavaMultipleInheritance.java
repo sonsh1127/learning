27314587c2b0cae0002f8251ec972ade0fd18117
shell.son <shell.son@kakaocorp.com>
2018-08-04 15:57:52 +0900

    test code 추가

---
+++ java-module/src/test/java/concurrent/JavaMultipleInheritance.java
@@ -0,0 +1,33 @@
+package concurrent;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaMultipleInheritance {
+
+    public static void main(String[] args) {
+        Myclass myClass = new Myclass();
+        myClass.doWork("");
+
+        List<Integer> list = Arrays.asList(33, 3, 209);
+
+        int val = list.stream().mapToInt(value -> value).sum();
+
+    }
+}
+
+
+interface Increment {
+    default void doWork(String name){
+        System.out.println("dowork");
+    }
+}
+class Myclass implements Increment{
+
+}
+
+interface Doubling extends Increment{
+    default void doWork(String name){
+        System.out.println("doubling");
+    }
+}
---
+++ java-module/src/test/java/concurrent/Philosopher.java
@@ -0,0 +1,59 @@
+package concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Philosopher extends Thread {
+
+    private Chopstick left, right;
+
+    public Philosopher(Chopstick left, Chopstick right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public static void main(String[] args) {
+        int count = 5;
+        List<Philosopher> philosophers = new ArrayList<>(count);
+        List<Chopstick> chopsticks = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            chopsticks.add(new Chopstick());
+        }
+        for (int i = 0; i < count; i++) {
+            philosophers.add(new Philosopher(chopsticks.get(i), chopsticks.get((i + 1) % count)));
+        }
+        philosophers.forEach(p -> p.start());
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                think();
+                synchronized (left) {
+                    System.out.println("left chopsitck picked!! " + Thread.currentThread().getName());
+                    synchronized (right) {
+                        eat();
+                    }
+                }
+            }
+        } catch (InterruptedException ie) {
+        }
+    }
+
+    private void eat() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(1);
+        System.out.println("Thread :" + Thread.currentThread().getName() + ", " + "eat");
+    }
+
+    private void think() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(1);
+        System.out.println("Thread :" + Thread.currentThread().getName() + ", " + "think");
+    }
+}
+
+
+class Chopstick {
+
+}
