0b57e8ded06abedb2da590a4f3993475d5f1ccaa
shell.son <shell.son@kakaocorp.com>
2019-02-25 23:08:03 +0900

    chapter 5 exercises added

--- scala-module/src/main/scala/fpbook/ch5/Stream.scala
+++ scala-module/src/main/scala/fpbook/ch5/Stream.scala
@@ -1,6 +1,6 @@
 package fpbook.ch5
 
-import fpbook.ch5.Stream.{cons, empty}
+import fpbook.ch5.Stream.{cons, empty, unfoldViaMatch}
 
 sealed trait Stream[+A] {
 
@@ -59,7 +59,7 @@ sealed trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
@@ -91,9 +91,73 @@ sealed trait Stream[+A] {
   }
 
   def headOptionViaFoldRight(): Option[A] = {
-    foldRight(None: Option[A])((a, op) => Some(a))
+    foldRight(None: Option[A])((a, _) => Some(a))
   }
 
+  def mapViaUnFold[B](f: A => B): Stream[B] = {
+    unfoldViaMatch(this)(s =>
+      s match {
+        case Cons(h, t) => Some((f(h()), t()))
+        case _ => None
+      }
+    )
+  }
+
+  def takeViaUnFold(n: Int): Stream[A] = {
+    // 인자에 묶어서 전달하는 방법
+    unfoldViaMatch((this, n)) {
+      case (Cons(h, t), 1) => Some((h(), (empty, 0)))
+      case (Cons(h, t), n) if n > 1 => Some((h(), (t(), n - 1)))
+      case _ => None
+    }
+  }
+
+  def takeWhileUnFold(f: A => Boolean): Stream[A] = {
+    unfoldViaMatch(this) {
+      case Cons(h, t) if (f(h())) => Some((h(), t()))
+      case _ => None
+    }
+  }
+
+  def zipWith[B, C](s2: Stream[B])(f: (A, B) => C): Stream[C] = {
+    unfoldViaMatch((this, s2)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(h1(), h2()), (t1(), t2()))
+      case _ => None
+    }
+  }
+
+  def zip[B](s2: Stream[B]): Stream[(A,B)] = zipWith(s2)((_, _))
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] =
+    unfoldViaMatch((this, s2)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+      case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), Empty))
+      case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (Empty, t2()))
+      case _ => None
+    }
+
+  def startsWith[A](s: Stream[A]): Boolean = {
+    zipAll(s)
+      .takeWhile(op => op._2 != None)
+      .forAll{case (x, y) => x == y}
+  }
+
+  def tails: Stream[Stream[A]] = {
+    // 내 꼬리는 전체 스트림의 꼬리가 맞다
+    val lastItems = unfoldViaMatch(this){
+      case Cons(h, t) => Some(t(), t())
+      case _ => None
+    }
+
+    cons(this, lastItems)
+  }
+
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight(cons(z, empty))((a, acc) => {
+      acc match {
+        case Cons(h, t) => cons(f(a, h()), acc)
+      }
+    })
 }
 
 case object Empty extends Stream[Nothing] {
@@ -117,6 +181,48 @@ object Stream {
   def apply[A](as: A*): Stream[A] = {
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
   }
+
+  def constant[A](a: A): Stream[A] = {
+    cons(a, constant(a))
+  }
+
+  def from(n: Int): Stream[Int] = {
+    cons(n, from(n + 1))
+  }
+
+  def fib(): Stream[Int] = {
+    def go(a: Int, b: Int): Stream[Int] = {
+      cons(a, go(b, a + b))
+    }
+
+    go(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z).map(tuple => cons(tuple._1, unfold(tuple._2)(f))).getOrElse(empty)
+  }
+
+  def unfoldViaMatch[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((a, s)) => cons(a, unfoldViaMatch(s)(f))
+      case None => empty
+    }
+  }
+
+  def fibsViaUnFold(): Stream[Int] = {
+    unfoldViaMatch((0, 1))(op => Some((op._1, (op._2, op._1 + op._2))))
+  }
+
+  def fromViaUnFold(n: Int): Stream[Int] = {
+    unfoldViaMatch(n)(a => Some(a, a + 1))
+  }
+
+  def constantViaUnFold(n: Int): Stream[Int] = {
+    unfoldViaMatch(n)(n => Some(n, n))
+  }
+
+  val onesViaUnfold = unfold(1)(_ => Some((1, 1)))
+
 }
 
 
--- scala-module/src/test/scala/fpbook/ch5/Ch5Exercises.scala
+++ scala-module/src/test/scala/fpbook/ch5/Ch5Exercises.scala
@@ -1,6 +1,6 @@
 package fpbook.ch5
 
-import fpbook.ch5.Stream.{cons, empty}
+import fpbook.ch5.Stream.{cons, constant, empty}
 import org.junit.Assert._
 import org.junit.{Assert, Test}
 
@@ -40,12 +40,12 @@ class Ch5Exercises {
 
   @Test
   def streamVia_cons() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     stream.headOption
     stream.headOption
   }
 
-  private def createStreamVia_cons = {
+  private def createStreamVia_cons123 = {
     cons({
       println("evaluated 1");
       1
@@ -58,7 +58,7 @@ class Ch5Exercises {
     }, empty)))
   }
 
-  private def createStreamVia_cons2 = {
+  private def createStreamVia_cons456 = {
     cons({
       println("evaluated 4");
       4
@@ -69,6 +69,23 @@ class Ch5Exercises {
       println("evaluated 6");
       6
     }, empty)))
+  }
+
+
+  private def createStreamVia_cons4567 = {
+    cons({
+      println("evaluated 4");
+      4
+    }, cons({
+      println("evaluated 5");
+      5
+    }, cons({
+      println("evaluated 6");
+      6
+    }, cons({
+      println("evaluated 7");
+      7
+    }, empty))))
   }
 
 
@@ -98,13 +115,22 @@ class Ch5Exercises {
   }
 
   @Test
+  def chaining() {
+    val res = createStreamVia_cons4567.mapViaFoldRight(x => {
+      println(x + 10)
+      x + 10
+    }
+    ).filterViaFoldRight(_ % 2 == 0)
+  }
+
+  @Test
   def prob1_toList() {
     assertEquals(List("a", "b", "c"), Stream("a", "b", "c").toList)
   }
 
   @Test
   def prob2_take() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     val taken = stream.take(2)
     println("taken")
     val expectedList = List(1, 2)
@@ -135,13 +161,13 @@ class Ch5Exercises {
 
   @Test
   def prob5_takeWhileViaFoldRight() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     stream.takeWhileViaFoldRight(it => it < 3)
   }
 
   @Test
   def prob6_headOptionViaFoldRight() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     val head = stream.headOptionViaFoldRight()
     println("actual")
     val head2 = stream.headOption
@@ -150,7 +176,7 @@ class Ch5Exercises {
 
   @Test
   def prob7_mapViaFoldRight() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     val res = stream.mapViaFoldRight(a => a * 2)
     println("actual")
     assertEquals(List(2, 4, 6), res.toList)
@@ -158,7 +184,7 @@ class Ch5Exercises {
 
   @Test
   def prob7_filterViaFoldRight() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     val res = stream.filterViaFoldRight(x => x % 2 == 1)
     println("actual")
     assertEquals(List(1, 3), res.toList)
@@ -166,21 +192,154 @@ class Ch5Exercises {
 
   @Test
   def prob7_appendViaFoldRight() {
-    val stream = createStreamVia_cons
-    val actual = stream.appendViaFoldRight(createStreamVia_cons2)
+    val stream = createStreamVia_cons123
+    val actual = stream.appendViaFoldRight(createStreamVia_cons456)
     println("actual")
     assertEquals(List(1, 2, 3) ++ List(4, 5, 6), actual.toList)
   }
 
   @Test
   def prob7_flatMapViaFoldRight() {
-    val stream = createStreamVia_cons
+    val stream = createStreamVia_cons123
     val actual = stream.flatMapViaFoldRight(x => {
-      createStreamVia_cons2
+      createStreamVia_cons456
     })
-
     println("actual")
-    assertEquals(List(4,5,6,4,5,6,4,5,6), actual.toList)
+    assertEquals(List(4, 5, 6, 4, 5, 6, 4, 5, 6), actual.toList)
+  }
+
+  // local variable does not support defni
+  val ones: Stream[Int] = cons(1, ones)
+
+  @Test
+  def infinite_stream() {
+    val fiveOnes = ones.take(5).toList
+    val expected = List.fill(5)(1)
+    assertEquals(expected, fiveOnes)
+    assertTrue(ones.exists(_ % 2 != 0))
+  }
+
+  @Test
+  def prob8_constant() {
+    assertEquals(List(2, 2, 2), constant(2).take(3).toList)
+  }
+
+  @Test
+  def prob9_from() {
+    assertEquals(List(3, 4, 5), Stream.from(3).take(3).toList)
+  }
+
+  @Test
+  def prob10_fibs() {
+    assertEquals(List(0, 1, 1, 2, 3, 5), Stream.fib().take(6).toList)
+  }
+
+  @Test
+  def prob11_unfold() {
+    val cons = Stream.unfoldViaMatch(1)(x => Some((1, 1)))
+    val ones = cons.take(3)
+    assertEquals(List(1, 1, 1), ones.toList)
+  }
+
+  @Test
+  def prob12_fibsViaUnfold() {
+    val actual = Stream.fibsViaUnFold().take(6).toList
+    assertEquals(List(0, 1, 1, 2, 3, 5), actual)
+  }
+
+  @Test
+  def prob12_fromViaUnfold() {
+    assertEquals(List(3, 4, 5), Stream.fromViaUnFold(3).take(3).toList)
+  }
+
+  @Test
+  def prob12_constantViaUnfold() {
+    assertEquals(List(2, 2, 2), Stream.constantViaUnFold(2).take(3).toList)
+  }
+
+  @Test
+  def prob12_onesViaUnfold() {
+    assertEquals(List(1, 1, 1), Stream.onesViaUnfold.take(3).toList)
+  }
+
+  @Test
+  def prob13_mapViaUnFold() {
+    val stream = createStreamVia_cons123
+    val res = stream.mapViaUnFold(a => a * 2)
+    println("actual")
+    assertEquals(List(2, 4, 6), res.toList)
+  }
+
+  @Test
+  def prob13_takeViaUnFold() {
+
+    val stream = createStreamVia_cons123
+    val taken = stream.takeViaUnFold(2)
+    println("taken")
+    val expectedList = List(1, 2)
+    assertEquals(expectedList, taken.toList)
+  }
+
+  @Test
+  def prob13_takeWhileViaUnFold() {
+    val stream = Stream(1, 3, 5, 8, 6)
+    val res = stream.takeWhileUnFold(_ % 2 == 1)
+    val expected = List(1, 3, 5)
+    assertEquals(expected, res.toList)
+  }
+
+  @Test
+  def prob13_zipWithViaUnFold() {
+    val stream = createStreamVia_cons123
+    val stream2 = createStreamVia_cons4567
+
+    val zipped = stream.zipWith(stream2)(_ + _)
+
+    assertEquals(3, size(zipped))
+    assertEquals(List(5, 7, 9), zipped.toList)
+  }
+
+  def size[A](s: Stream[A]): Int = {
+    s.foldRight(0)((_, count) => count + 1)
+  }
+
+  @Test
+  def prob13_zipAllViaUnFold() {
+    val stream = createStreamVia_cons123
+    val stream2 = createStreamVia_cons4567
+    val zipped = stream.zipAll(stream2)
+    assertEquals(4, size(zipped))
+    val expected = List(
+      (Some(1), Some(4)),
+      (Some(2), Some(5)),
+      (Some(3), Some(6)),
+      (None, Some(7))
+    )
+    assertEquals(expected, zipped.toList)
+  }
+
+  @Test
+  def prob14_startsWith() {
+    val stream = createStreamVia_cons123
+    assertTrue(stream.startsWith(Stream(1, 2)))
+    assertTrue(stream.startsWith(Stream(1)))
+    assertTrue(stream.startsWith(Stream(1, 2, 3)))
+    assertFalse(stream.startsWith(Stream(1, 1, 3)))
+  }
+
+  @Test
+  def prob15_tailsViaUnFold() {
+    val stream = createStreamVia_cons123
+    val actual = stream.tails
+    val listStream = actual.mapViaUnFold(s => s.toList)
+    assertEquals(List(List(1, 2, 3), List(2, 3), List(3), List()), listStream.toList)
+  }
+
+  @Test
+  def prob16_scanRight() {
+    val stream = createStreamVia_cons123
+    val res = stream.scanRight(0)(_ + _).toList
+    assertEquals(List(6, 5, 3, 0), res)
   }
 
 }
